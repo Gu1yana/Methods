bf1bfe0880ca338c1178d297da75c6a191d188b6
Gu1yana <gulyanamk-bp110@code.edu.az>
2025-03-14 03:49:43 +0400

    metodtask2

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -1,22 +1,56 @@
-﻿using System;
+﻿//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("metni daxil edin:");
+//        string text = Console.ReadLine();
+//        string reversedtext = ReverseText(text);
+//        Console.WriteLine($"tersine cevrilmis metn:{reversedtext}");
+//    }
+//    static string ReverseText(string input)
+//    {
+//        string reverse = "";
+
+//        for (int i = input.Length - 1; i >= 0; i--)
+//        {
+//            reverse += input[i];
+
+//        }
+//        return reverse;
+//    }
+//}
+
+
+
+
+using System;
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("metni daxil edin:");
-        string text = Console.ReadLine();
-        string reversedtext = ReverseText(text);
-        Console.WriteLine($"tersine cevrilmis metn:{reversedtext}");
+        Console.WriteLine("ay sayini daxil edin:");
+        int ay = Convert.ToInt32(Console.ReadLine());
+        string netice = AyVeIleCevir(ay);
+        Console.WriteLine(netice);
     }
-    static string ReverseText(string input)
-    {
-        string reverse = "";
 
-        for (int i = input.Length - 1; i >= 0; i--)
+    static string AyVeIleCevir(int ay)
+    {
+        int il = ay / 12;
+        int qaliqAy = ay % 12;
+        string netice = il + "il" + qaliqAy + "ay";
+        if (il>0 && qaliqAy>0)
         {
-            reverse += input[i];
-            
+            return netice;
         }
-        return reverse;
+        else if(il>0)
+        {
+            return il+"il" ;
+        }
+        else
+        {
+            return  ay+"ay";
+        }
     }
 }
--- Methods/Methods/obj/Debug/net8.0/Methods.AssemblyInfo.cs
+++ Methods/Methods/obj/Debug/net8.0/Methods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2419cfc7af59d507e11ee3f02d0b3f7020e5131")]
 [assembly: System.Reflection.AssemblyProductAttribute("Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
