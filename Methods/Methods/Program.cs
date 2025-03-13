c2419cfc7af59d507e11ee3f02d0b3f7020e5131
Gu1yana <gulyanamk-bp110@code.edu.az>
2025-03-14 03:23:14 +0400

    metodtask1

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("metni daxil edin:");
+        string text = Console.ReadLine();
+        string reversedtext = ReverseText(text);
+        Console.WriteLine($"tersine cevrilmis metn:{reversedtext}");
+    }
+    static string ReverseText(string input)
+    {
+        string reverse = "";
+
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            reverse += input[i];
+            
+        }
+        return reverse;
+    }
+}
