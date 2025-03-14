1495a06580feca20756fdd97329e0b422af55d34
Gu1yana <gulyanamk-bp110@code.edu.az>
2025-03-14 04:08:23 +0400

    metodtask3

--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -24,33 +24,58 @@
 
 
 
+//using System;
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("ay sayini daxil edin:");
+//        int ay = Convert.ToInt32(Console.ReadLine());
+//        string netice = AyVeIleCevir(ay);
+//        Console.WriteLine(netice);
+//    }
+
+//    static string AyVeIleCevir(int ay)
+//    {
+//        int il = ay / 12;
+//        int qaliqAy = ay % 12;
+//        string netice = il + "il" + qaliqAy + "ay";
+//        if (il>0 && qaliqAy>0)
+//        {
+//            return netice;
+//        }
+//        else if(il>0)
+//        {
+//            return il+"il" ;
+//        }
+//        else
+//        {
+//            return  ay+"ay";
+//        }
+//    }
+//}
+
+
 using System;
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("ay sayini daxil edin:");
-        int ay = Convert.ToInt32(Console.ReadLine());
-        string netice = AyVeIleCevir(ay);
+        Console.WriteLine("ededi daxil edin:");
+        int eded = Convert.ToInt32(Console.ReadLine());
+        int netice = FaktoriyalTap(eded);
+        Console.WriteLine("daxil etdiyiniz ededin faktoriyali:");
         Console.WriteLine(netice);
     }
-
-    static string AyVeIleCevir(int ay)
+    static int FaktoriyalTap(int eded)
     {
-        int il = ay / 12;
-        int qaliqAy = ay % 12;
-        string netice = il + "il" + qaliqAy + "ay";
-        if (il>0 && qaliqAy>0)
+        int hasil = 1;
+
+        for (int i = 1; i <= eded; i++)
         {
-            return netice;
+            hasil *= i;
+
         }
-        else if(il>0)
-        {
-            return il+"il" ;
-        }
-        else
-        {
-            return  ay+"ay";
-        }
+        return hasil;
     }
 }
